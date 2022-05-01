29f085e9d1ba0b77e90122f0ead048d3590f2455
Theodore Ayoub <theodore.ayoub@gmail.com>
2022-04-30 20:00:39 -0700

    city input feeds into API, JSON prints in console

--- assets/script.js
+++ assets/script.js
@@ -3,73 +3,48 @@ var APIKey = "9a721b9d54b0807c9597675727d44009";
 // print out the JSON file with the weather info
 var weatherContainer = document.querySelector('#weather-container');
 var cityE1 = document.querySelector('.cityName');
-var _locationInput = document.querySelector("#location");
+var locationInputEl = document.querySelector("#location");
 var submitButton = document.querySelector("#submit");
+
 
 submitButton.addEventListener("click", function(event) {
   event.preventDefault();
-  
   // create user object from submission
   var userInput = {
-    location: _locationInput.value.trim(),
+    location: locationInputEl.value.trim(),
   };
-
   // set new submission to local storage 
-  localStorage.setItem("userInput", JSON.stringify(userInput));
-  
+  localStorage.setItem('userInput', JSON.stringify(userInput));
+
+  // run the function to get the weather data
+  getWeatherData(APIKey);
 });
 
 
-
-// https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
-
-
-var getWeatherData = function (city, APIKey) {
+var getWeatherData = function (APIKey) {
+    var city = locationInputEl.value.trim();
+    console.log("city", city);
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
     fetch(queryURL)
     .then(function (response) {
-        if (response.ok) {
-          console.log(response);
-          response.json().then(function (data) {
-            console.log(data);
-            displayWeather(data, city);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert('Unable to connect');
-      });
+      return response.json();
+    }).then(function (obj) {
+      console.log(obj);
+    }).catch(function (error) {
+      console.error("something went wrong!");
+      console.error(error);
+    });
 };
 
-var displayWeather = function (weather, searchTerm) {
-    if (weather.length === 0) {
-      weatherContainer.textContent = 'No weather data found.';
-      // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
-      return;
-    }
-}
 
-
-
-  // https://stackoverflow.com/questions/51851391/fetch-json-data-from-api-javascript
-
-
-// var city = 'san diego';
-// console.log(city);
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-// console.log(queryURL);
-// fetch(queryURL)
-// .then(function (response) {
-//   return response.json();
-// }).then(function (obj) {
-//   console.log(obj);
-// }).catch(function (error) {
-//   console.error("something went wrong!");
-//   console.error(error);
-// });
+// var displayWeather = function (weather, searchTerm) {
+//   if (weather.length === 0) {
+//     weatherContainer.textContent = 'No weather data found.';
+//     // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
+//     return;
+//   }
+// }
 
 
 
@@ -78,6 +53,7 @@ var displayWeather = function (weather, searchTerm) {
 2. enable feature for when they press the enter key.
 3. print the city name to the webpage
 4. use the city name as input into the API
+
 5. print something from the API onto the screen
 6. print out the info I want from the API onto the screen
 7. add localstorage for the city name, so the data remains persistent
