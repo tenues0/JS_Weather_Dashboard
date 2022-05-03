9693726e91c339b75ae72c3e47487ff296b6a5f5
Theodore Ayoub <theodore.ayoub@gmail.com>
2022-05-03 14:21:00 -0700

    current day forecast printed to screen

--- assets/script.js
+++ assets/script.js
@@ -37,23 +37,23 @@ var displayForecast = function (obj) {
   divName.innerHTML = name;
   nameDiv.appendChild(divName);
 
-  var temp = obj.list[0].main.temp;
-  var tempDiv = document.getElementById("temp");
-  var div = document.createElement("h4");
-  div.innerHTML = temp;
-  tempDiv.appendChild(div);
+  // var temp = obj.list[0].main.temp;
+  // var tempDiv = document.getElementById("temp");
+  // var div = document.createElement("h4");
+  // div.innerHTML = temp;
+  // tempDiv.appendChild(div);
 
-  var wind = obj.list[0].wind.speed;
-  var windDiv = document.getElementById("wind");
-  var divwind = document.createElement("h4");
-  divwind.innerHTML = wind;
-  windDiv.appendChild(divwind);
+  // var wind = obj.list[0].wind.speed;
+  // var windDiv = document.getElementById("wind");
+  // var divwind = document.createElement("h4");
+  // divwind.innerHTML = wind;
+  // windDiv.appendChild(divwind);
 
-  var humidity = obj.list[0].main.humidity;
-  var humidityDiv = document.getElementById("humidity");
-  var divHumidity = document.createElement("h4");
-  divHumidity.innerHTML = humidity;
-  humidityDiv.appendChild(divHumidity);
+  // var humidity = obj.list[0].main.humidity;
+  // var humidityDiv = document.getElementById("humidity");
+  // var divHumidity = document.createElement("h4");
+  // divHumidity.innerHTML = humidity;
+  // humidityDiv.appendChild(divHumidity);
 
   var latCoordinate = obj.city.coord.lat;
   console.log("lat coord ", latCoordinate);
@@ -82,22 +82,46 @@ var currentWeatherData = function (APIKey, latCoordinate, lonCoordinate) {
     return response.json();
   }).then(function (data) {
     console.log(data);
-    // displayCurrent(data);
+    displayCurrent(data);
   }).catch(function (error) {
     console.error("Emotional Damage! currentWeatherData failure!");
     console.error(error);
   });
 };
 
-
-
 var displayCurrent = function (data) {
 
-  var uvi = data.current.uvi;
+  var dateToday = new Date(1651611941 * 1000).toLocaleDateString("en-US");
+  console.log("the date is ", dateToday);
+  var dateDiv = document.getElementById("date");
+  var divDate = document.createElement("p");
+  divDate.innerHTML = dateToday;
+  dateDiv.appendChild(divDate);
+
+  var temp = data.current.temp;
+  var tempDiv = document.getElementById("temp");
+  var div = document.createElement("p");
+  div.innerHTML = temp;
+  tempDiv.appendChild(div);
+
+  var wind = data.current.wind_speed;
+  var windDiv = document.getElementById("wind");
+  var divwind = document.createElement("p");
+  divwind.innerHTML = wind;
+  windDiv.appendChild(divwind);
+
+  var humidity = data.current.humidity;
+  var humidityDiv = document.getElementById("humidity");
+  var divHumidity = document.createElement("p");
+  divHumidity.innerHTML = humidity;
+  humidityDiv.appendChild(divHumidity);
+
+  var uviIndex = data.current.uvi;
   var uviDiv = document.getElementById("uvindex");
-  var divUVI = document.createElement("h4");
-  divUVI.innerHTML = uvi;
+  var divUVI = document.createElement("p");
+  divUVI.innerHTML = uviIndex;
   uviDiv.appendChild(divUVI);
+
 };
 
 
@@ -118,17 +142,3 @@ submitButton.addEventListener("click", function(event) {
   
 });
 
-/*
-try using template literals
-
-1. get info when user enters in a city name.
-2. enable feature for when they press the enter key.
-3. print the city name to the webpage
-4. use the city name as input into the API
-
-5. print something from the API onto the screen
-  What from the API needs to written to the screen?
-6. print out the info I want from the API onto the screen
-7. add localstorage for the city name, so the data remains persistent
-8. 
- */
