89180b5cf3b9889b0b5d15813e1ec85de4d55f8c
Theodore Ayoub <theodore.ayoub@gmail.com>
2022-05-04 15:59:17 -0700

    publishing first forecast day to page

--- assets/script.js
+++ assets/script.js
@@ -63,12 +63,33 @@ var displayForecast = function (obj) {
   divDay1.innerHTML = day1Date;
   day1Div.appendChild(divDay1);
 
-  var day2Date = new Date(obj.list[2].dt * 1000).toLocaleDateString("en-US");
-  console.log("the day2Date is ", day2Date);
-  var day2Div = document.getElementById("day2");
-  var divDay2 = document.createElement("p");
-  divDay2.innerHTML = day2Date;
-  day2Div.appendChild(divDay2);
+  var day1Temp = obj.list[1].main.temp;
+  console.log("day1Temp ", day1Temp);
+  var temp1Div = document.getElementById("day1");
+  var divTemp1 = document.createElement("p");
+  divTemp1.innerHTML = day1Temp;
+  temp1Div.appendChild(divTemp1);
+
+  var day1Wind = obj.list[1].wind.speed;
+  console.log("day1Wind ", day1Wind);
+  var wind1Div = document.getElementById("day1");
+  var divWind1 = document.createElement("p");
+  divWind1.innerHTML = day1Wind;
+  wind1Div.appendChild(divWind1);
+
+  var day1Hum = obj.list[1].main.humidity;
+  console.log("day1Hum ", day1Hum);
+  var hum1Div = document.getElementById("day1");
+  var divHum1 = document.createElement("p");
+  divHum1.innerHTML = day1Hum;
+  hum1Div.appendChild(divHum1);
+
+  // var day2Date = new Date(obj.list[2].dt * 1000).toLocaleDateString("en-US");
+  // console.log("the day2Date is ", day2Date);
+  // var day2Div = document.getElementById("day2");
+  // var divDay2 = document.createElement("p");
+  // divDay2.innerHTML = day2Date;
+  // day2Div.appendChild(divDay2);
 }
 
 var currentWeatherData = function (APIKey, latCoordinate, lonCoordinate) {
@@ -91,7 +112,7 @@ var currentWeatherData = function (APIKey, latCoordinate, lonCoordinate) {
 var displayCurrent = function (data) {
 
   var dateToday = new Date(data.current.dt * 1000).toLocaleDateString("en-US");
-  console.log("the date is ", dateToday);
+  console.log("dateToday ", dateToday);
   var dateDiv = document.getElementById("date");
   var divDate = document.createElement("p");
   divDate.innerHTML = dateToday;
@@ -127,21 +148,21 @@ var displayCurrent = function (data) {
 // trying to save the user history and create buttons
 var userInputHistory;
 
-//   if (localStorage.getItem("userInputHistory")) {
-//      userInputHistory = JSON.parse(localStorage.getItem("userInputHistory"));
-//   } else {
-//      userInputHistory = [];
-//   };
-// console.log(typeof userInputHistory)
-// console.log( userInputHistory)
+  if (localStorage.getItem("userInputHistory")) {
+     userInputHistory = JSON.parse(localStorage.getItem("userInputHistory"));
+  } else {
+     userInputHistory = [];
+  };
+console.log(typeof userInputHistory)
+console.log( userInputHistory)
 
-// for (let i = 0; i < userInputHistory.length; i++) {
-//   const button = document.createElement("button");
-//   inputHistoryButton.append(button);
-//   button.innerHTML = userInputHistory[i];
-//   button.classList.add("button-history");
+for (let i = 0; i < userInputHistory.length; i++) {
+  const button = document.createElement("button");
+  inputHistoryButton.append(button);
+  button.innerHTML = userInputHistory[i];
+  button.classList.add("button-history");
   
-// }
+}
 
 // var histBtn = $(".button-history")
 // histBtn.click(funciton(e){
@@ -163,103 +184,18 @@ var userInputHistory;
 //     });
 // };
 
-// // https://w3collective.com/fetch-display-api-data-javascript/
-// var displayForecast = function (obj) {
-
-//   // Using the forecast API to get the city name and coordinates
-//   // for the onecall API
-//   var name = obj.city.name;
-//   var nameDiv = document.getElementById("cityName");
-//   var divName = document.createElement("h4");
-//   divName.innerHTML = name;
-//   nameDiv.appendChild(divName);
-
-//   var latCoordinate = obj.city.coord.lat;
-//   console.log("lat coord ", latCoordinate);
-//   var latCoord = document.getElementById("lat");
-//   var coordLat = document.createElement("p");
-//   coordLat.innerHTML = latCoordinate;
-//   latCoord.appendChild(coordLat);
-
-//   var lonCoordinate = obj.city.coord.lon;
-//   console.log("lon coord ", lonCoordinate);
-//   var lonCoord = document.getElementById("lon");
-//   var coordLon = document.createElement("p");
-//   coordLon.innerHTML = lonCoordinate;
-//   lonCoord.appendChild(coordLon);
-
-//   // sending the coordinates into the onecall API
-//   currentWeatherData(APIKey, latCoordinate, lonCoordinate);
-
-//   var day1Date = new Date(obj.list[1].dt * 1000).toLocaleDateString("en-US");
-//   console.log("the day1Date is ", day1Date);
-//   var day1Div = document.getElementById("day1");
-//   var divDay1 = document.createElement("p");
-//   divDay1.innerHTML = day1Date;
-//   day1Div.appendChild(divDay1);
-// }
-
-// var currentWeatherData = function (APIKey, latCoordinate, lonCoordinate) {
-//   console.log("lat coord within onecall API ", latCoordinate);
-//   console.log("lon coord within onecall API ", lonCoordinate);
-//   var queryURL = "http://api.openweathermap.org/data/2.5/onecall?" + "lat=" + latCoordinate + "&lon=" + lonCoordinate + "&exclude=minutely,hourly,daily,alerts" + "&appid=" + APIKey + "&units=imperial";
-
-//   fetch(queryURL)
-//     .then(function (response) {
-//       return response.json();
-//     }).then(function (data) {
-//       console.log(data);
-//       displayCurrent(data);
-//     }).catch(function (error) {
-//       console.error("Emotional Damage! currentWeatherData failure!");
-//       console.error(error);
-//     });
-// };
-
-// var displayCurrent = function (data) {
-
-//   var dateToday = new Date(data.current.dt * 1000).toLocaleDateString("en-US");
-//   console.log("the date is ", dateToday);
-//   var dateDiv = document.getElementById("date");
-//   var divDate = document.createElement("p");
-//   divDate.innerHTML = dateToday;
-//   dateDiv.appendChild(divDate);
-
-//   var temp = data.current.temp;
-//   var tempDiv = document.getElementById("temp");
-//   var div = document.createElement("p");
-//   div.innerHTML = temp;
-//   tempDiv.appendChild(div);
-
-//   var wind = data.current.wind_speed;
-//   var windDiv = document.getElementById("wind");
-//   var divwind = document.createElement("p");
-//   divwind.innerHTML = wind;
-//   windDiv.appendChild(divwind);
-
-//   var humidity = data.current.humidity;
-//   var humidityDiv = document.getElementById("humidity");
-//   var divHumidity = document.createElement("p");
-//   divHumidity.innerHTML = humidity;
-//   humidityDiv.appendChild(divHumidity);
-
-//   var uviIndex = data.current.uvi;
-//   var uviDiv = document.getElementById("uvindex");
-//   var divUVI = document.createElement("p");
-//   divUVI.innerHTML = uviIndex;
-//   uviDiv.appendChild(divUVI);
-// })
 
 submitButton.addEventListener("click", function (event) {
   event.preventDefault();
+
   // create user object from submission
 
-  // var userInput = {
-  //   location: locationInputEl.value.trim(),
-  // };
-  // // userInputHistory.push(userInput.location);
-  // // // set new submission to local storage 
-  // // localStorage.setItem('userInputHistory', JSON.stringify(userInputHistory));
+  var userInput = {
+    location: locationInputEl.value.trim(),
+  };
+  userInputHistory.push(userInput.location);
+  // set new submission to local storage 
+  localStorage.setItem('userInputHistory', JSON.stringify(userInputHistory));
 
   // run the function to get the weather data
   forecastWeatherData(APIKey);
@@ -274,11 +210,23 @@ submitButton.addEventListener("click", function (event) {
 
 
 /*
+Problems:
+1. the date from the forecast API call is off
+line 59
+
+2. when a new city is inputted it lays data over the old, the page does not reset.
+Need to find a way to clear out the old so when the new data publishes it remains on top.
+
+3. history buttons only show up when the page is refreshed.
+
+todo list:
 Put 5-day forecast into the html
 
 localstorage for each day
 
 create buttons for each city searched
+
+learn about putting icons in
 
 CSS styling
 
